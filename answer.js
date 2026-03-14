a1a3df41e9d4f2c85bf1eade137df78920f1b1bb
Kaustubh Gongnur <kaustubhgongnur@gmail.com>
2026-03-14 12:58:10 +0530

    Create answer.js
    javascript test 0001

---
+++ answer.js
@@ -0,0 +1,41 @@
+//# javascript-test-0001-final-14703-kaustubh
+//Final Project Assignment - This repository contains the complete final project code and documentation.
+
+let n = 5;
+for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
+    }
+    
+    for (let j = 1; j <= (2 * i - 1); j++) {
+        if (j === 1 || j === (2 * i - 1)) {
+            row += "*";
+        } else {
+            row += " ";
+        }
+    }
+
+    console.log(row);
+}
+
+
+for (let i = n - 1; i >= 1; i--) {
+    let row = "";
+
+
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
+    }
+
+    
+    for (let j = 1; j <= (2 * i - 1); j++) {
+        if (j === 1 || j === (2 * i - 1)) {
+            row += "*";
+        } else {
+            row += " ";
+        }
+    }
+
+    console.log(row);
+}
